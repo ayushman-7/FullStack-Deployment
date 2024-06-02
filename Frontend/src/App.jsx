c07f4942fddbb7d277e3e803d848e403346a6c68
ayushman-7 <21051553@kiit.ac.in>
2024-06-02 23:37:30 +0530

    final commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [jokes,setJokes]=useState([])
+
+  useEffect(() => {
+    axios.get('https://localhost:3000/jokes')
+      .then(response => {
+        setJokes(response.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
+  return (
+    <>
+  <h1>FULLSTACK </h1>
+  <p>JOKES: {jokes.length}</p>
+
+  {
+    jokes.map((joke,index) => {
+
+      <div key={joke.id}>
+        <p>{joke.content}</p>
+      </div>
+      // code to render each joke
+    })
+  }
+    </>
+  )
+}
+
+export default App
+function newFunction() {
+  useEffect()
+}
+
